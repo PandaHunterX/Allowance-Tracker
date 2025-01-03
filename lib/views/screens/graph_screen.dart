041f777cead714edd4b2f349a44d998d73b27cbf
PandaHunterX <147257813+PandaHunterX@users.noreply.github.com>
2025-01-03 21:44:22 +0800

    Weekly Graph for both Expense and Allowance

--- lib/views/screens/graph_screen.dart
+++ lib/views/screens/graph_screen.dart
@@ -1,15 +1,12 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:productivity_app/models/allowance_item.dart';
 
 import '../../database/finance_db.dart';
 import '../../models/expense_item.dart';
 
 class GraphScreen extends StatefulWidget {
-  GraphScreen({super.key});
-
-  final Color leftBarColor = Colors.blue;
-  final Color rightBarColor = Colors.red;
-  final Color avgColor = Colors.orange;
+  const GraphScreen({super.key});
 
   @override
   State<StatefulWidget> createState() => GraphScreenState();
@@ -17,45 +14,64 @@ class GraphScreen extends StatefulWidget {
 
 class GraphScreenState extends State<GraphScreen> {
   final double width = 15;
+  String _selectedType = 'Expense';
 
+  late Future<void> _loadDataFuture;
   late List<BarChartGroupData> rawBarGroups;
   late List<BarChartGroupData> showingBarGroups;
-  double maxExpense = 0;
+  double maxValue = 0;
 
   int touchedGroupIndex = -1;
 
   @override
   void initState() {
     super.initState();
-    _loadData();
+    _loadDataFuture = _loadData();
   }
 
   Future<void> _loadData() async {
     final financeDB = FinanceDB();
-    final expenses = await financeDB.fetchExpense();
-    final weeklyExpenses = _processExpenses(expenses);
+    final data = _selectedType == 'Expense'
+        ? (await financeDB.fetchExpense())
+        : (await financeDB.fetchAllowance());
+    final weeklyData = _selectedType == 'Expense'
+        ? _processExpenses(data as List<ExpenseItem>)
+        : _processAllowance(data as List<AllowanceItem>);
 
     setState(() {
-      maxExpense = weeklyExpenses.reduce((a, b) => a > b ? a : b);
-      rawBarGroups = _generateBarGroups(weeklyExpenses);
+      maxValue = weeklyData.reduce((a, b) => a > b ? a : b);
+      rawBarGroups = _generateBarGroups(weeklyData);
       showingBarGroups = rawBarGroups;
     });
   }
 
   List<double> _processExpenses(List<ExpenseItem> expenses) {
-    final List<double> weeklyExpenses = List.filled(7, 0.0);
+    final List<double> weeklyData = List.filled(7, 0.0);
 
     for (var expense in expenses) {
-      final dayOfWeek = expense.dateTime.weekday - 1; // Monday is 0, Sunday is 6
-      weeklyExpenses[dayOfWeek] += expense.expense;
+      final dayOfWeek =
+          expense.dateTime.weekday - 1; // Monday is 0, Sunday is 6
+      weeklyData[dayOfWeek] += expense.expense;
     }
 
-    return weeklyExpenses;
+    return weeklyData;
+  }
+
+  List<double> _processAllowance(List<AllowanceItem> allowances) {
+    final List<double> weeklyData = List.filled(7, 0.0);
+
+    for (var allowance in allowances) {
+      final dayOfWeek =
+          allowance.dateTime.weekday - 1; // Monday is 0, Sunday is 6
+      weeklyData[dayOfWeek] += allowance.amount;
+    }
+
+    return weeklyData;
   }
 
   List<BarChartGroupData> _generateBarGroups(List<double> weeklyExpenses) {
     return List.generate(7, (index) {
-      return makeGroupData(index, weeklyExpenses[index], 0);
+      return makeGroupData(index, weeklyExpenses[index]);
     });
   }
 
@@ -65,207 +81,124 @@ class GraphScreenState extends State<GraphScreen> {
       aspectRatio: 1,
       child: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                makeTransactionsIcon(),
-                const SizedBox(
-                  width: 38,
-                ),
-                const Text(
-                  'Weekly Expenses',
-                  style: TextStyle(color: Colors.black, fontSize: 22),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 38,
-            ),
-            Expanded(
-              child: BarChart(
-                BarChartData(
-                  maxY: maxExpense, // Adjusted for better scaling
-                  barTouchData: BarTouchData(
-                    touchTooltipData: BarTouchTooltipData(
-                      getTooltipItem: (group, groupIndex, rod, rodIndex) {
-                        String day;
-                        switch (group.x.toInt()) {
-                          case 0:
-                            day = 'Mon';
-                            break;
-                          case 1:
-                            day = 'Tue';
-                            break;
-                          case 2:
-                            day = 'Wed';
-                            break;
-                          case 3:
-                            day = 'Thu';
-                            break;
-                          case 4:
-                            day = 'Fri';
-                            break;
-                          case 5:
-                            day = 'Sat';
-                            break;
-                          case 6:
-                            day = 'Sun';
-                            break;
-                          default:
-                            day = '';
-                        }
-                        return BarTooltipItem(
-                          '$day\n',
-                          const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: (rod.toY).toString(),
-                              style: const TextStyle(
-                                color: Colors.yellow,
-                                fontWeight: FontWeight.w500,
-                              ),
+        child: FutureBuilder<void>(
+          future: _loadDataFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error loading data'));
+            } else {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 38,
+                      ),
+                      const Text(
+                        'Weekly Data',
+                        style: TextStyle(color: Colors.black, fontSize: 22),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RadioListTile(
+                          title: const Text('Expense'),
+                          selected: true,
+                          value: 'Expense',
+                          groupValue: _selectedType,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedType = value!;
+                              _loadDataFuture = _loadData();
+                            });
+                          }),
+                      RadioListTile(
+                          title: const Text('Allowance'),
+                          value: 'Allowance',
+                          groupValue: _selectedType,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedType = value!;
+                              _loadDataFuture = _loadData();
+                            });
+                          })
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 38,
+                  ),
+                  Expanded(
+                    child: BarChart(
+                      BarChartData(
+                        maxY: maxValue,
+                        titlesData: FlTitlesData(
+                          show: true,
+                          bottomTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: true,
+                              getTitlesWidget: bottomTitles,
+                              reservedSize: 42,
                             ),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
-                  titlesData: FlTitlesData(
-                    show: true,
-                    rightTitles: const AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
-                    ),
-                    topTitles: const AxisTitles(
-                      sideTitles: SideTitles(showTitles: false),
-                    ),
-                    bottomTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: true,
-                        getTitlesWidget: bottomTitles,
-                        reservedSize: 42,
-                      ),
-                    ),
-                    leftTitles: AxisTitles(
-                      sideTitles: SideTitles(
-                        showTitles: true,
-                        reservedSize: 28,
-                        interval: (maxExpense / 5).ceilToDouble(), // Adjusted interval for better scaling
-                        getTitlesWidget: leftTitles,
+                          ),
+                          rightTitles: AxisTitles(
+                            sideTitles: SideTitles(
+                              showTitles: false,
+                            ),
+                          ),
+                          topTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false),
+                          ),
+                        ),
+                        borderData: FlBorderData(
+                          show: true,
+                        ),
+                        barGroups: showingBarGroups,
                       ),
                     ),
                   ),
-                  borderData: FlBorderData(
-                    show: false,
+                  const SizedBox(
+                    height: 12,
                   ),
-                  barGroups: showingBarGroups,
-                  gridData: const FlGridData(show: false),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-          ],
+                ],
+              );
+            }
+          },
         ),
       ),
-    );
-  }
-
-  Widget leftTitles(double value, TitleMeta meta) {
-    const style = TextStyle(
-      color: Color(0xff7589a2),
-      fontWeight: FontWeight.bold,
-      fontSize: 14,
-    );
-    String text = '${(value / 1000).round()}K';
-    return SideTitleWidget(
-      axisSide: meta.axisSide,
-      space: 0,
-      child: Text(text, style: style),
     );
   }
 
   Widget bottomTitles(double value, TitleMeta meta) {
     final titles = <String>['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-    final Widget text = Text(
-      titles[value.toInt()],
-      style: const TextStyle(
-        color: Color(0xff7589a2),
-        fontWeight: FontWeight.bold,
-        fontSize: 14,
-      ),
-    );
-
     return SideTitleWidget(
       axisSide: meta.axisSide,
       space: 16, // Margin top
-      child: text,
+      child: Text(
+        titles[value.toInt()],
+        style: const TextStyle(
+          color: Color(0xff7589a2),
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+        ),
+      ),
     );
   }
 
-  BarChartGroupData makeGroupData(int x, double y1, double y2) {
+  BarChartGroupData makeGroupData(int x, double y1) {
     return BarChartGroupData(
-      barsSpace: 4,
       x: x,
       barRods: [
         BarChartRodData(
           toY: y1,
-          color: widget.leftBarColor,
+          color: _selectedType == 'Expense' ? Colors.red : Colors.blue,
           width: width,
-        ),
-      ],
-    );
-  }
-
-  Widget makeTransactionsIcon() {
-    const width = 4.5;
-    const space = 3.5;
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Container(
-          width: width,
-          height: 10,
-          color: Colors.black.withOpacity(0.4),
-        ),
-        const SizedBox(
-          width: space,
-        ),
-        Container(
-          width: width,
-          height: 28,
-          color: Colors.black.withOpacity(0.8),
-        ),
-        const SizedBox(
-          width: space,
-        ),
-        Container(
-          width: width,
-          height: 42,
-          color: Colors.black,
-        ),
-        const SizedBox(
-          width: space,
-        ),
-        Container(
-          width: width,
-          height: 28,
-          color: Colors.black.withOpacity(0.8),
-        ),
-        const SizedBox(
-          width: space,
-        ),
-        Container(
-          width: width,
-          height: 10,
-          color: Colors.black.withOpacity(0.4),
         ),
       ],
     );
